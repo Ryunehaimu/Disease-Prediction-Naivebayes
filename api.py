56ec2a951bc4786e45aa89e124f13907719bf524
Ryunehaimu <ryunehaimu007@gmail.com>
2024-06-07 11:33:12 +0700

    publicurl

--- api.py
+++ api.py
@@ -1,19 +1,13 @@
-from flask import Flask, request, render_template, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
-CORS(app)  # Enables CORS for all routes
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    gejala_keys = ['gejala1', 'gejala2', 'gejala3', 'gejala4', 'gejala5']
-    gejala_values = [request.form.get(key) for key in gejala_keys if request.form.get(key)]
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json  # Mendapatkan data JSON dari permintaan POST
+    gejala_values = data.get('gejala', [])  # Mendapatkan gejala dari data JSON
     
     if len(gejala_values) < 3:
         return jsonify({"error": "Anda harus memasukkan minimal 3 gejala"}), 400
@@ -22,25 +16,25 @@ def submit():
     df = pd.read_csv('new_data.csv')
     model = joblib.load('health_model.pkl')
     
-    # Prepare the data for prediction
+    # Persiapkan data untuk prediksi
     df['prognosis'] = ''
     df = df.applymap(lambda x: 0)
     for gejala in gejala_values:
         if gejala in df.columns:
             df.loc[:, gejala] = 1
 
-    # Make predictions
+    # Lakukan prediksi
     new_predictions = model.predict(df.drop('prognosis', axis=1))
     df['prognosis'] = new_predictions
     df.to_csv('new_data.csv', index=False)
 
-    # Create response data
+    # Buat data respons
     result_data = {
-        "prognosis": new_predictions.tolist(),  # Convert numpy array to list for JSON serialization
+        "prognosis": new_predictions.tolist(),  # Konversi array numpy ke list untuk serialisasi JSON
         "selected_symptoms": gejala_values
     }
 
     return jsonify(result_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)
